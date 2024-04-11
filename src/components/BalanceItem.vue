225f46136d61ff24afb9aa2c01fde5073f33fd9a
FilipLeTran <filipletran@gmail.com>
2024-04-11 09:03:27 +0200

    add container with balance event

---
+++ src/components/BalanceItem.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  eventId?: string
+  date?: string
+  customerId?: string
+  balanceValue?: number
+  eventType?: string
+}>()
+
+const balanceContainerClass = ref('balance-container')
+</script>
+
+<template>
+  <div
+    :style="[eventType === 'INCREASED' ? { borderColor: 'green' } : { borderColor: 'red' }]"
+    :class="balanceContainerClass"
+  >
+    <div>
+      <h3>Event ID</h3>
+      <p>{{ eventId }}</p>
+    </div>
+    <div>
+      <h3>Date</h3>
+      <p>{{ date }}</p>
+    </div>
+    <div>
+      <h3>Customer ID</h3>
+      <p>{{ customerId }}</p>
+    </div>
+    <div>
+      <h3>Adjustment</h3>
+      <p>{{ balanceValue }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BalanceItem'
+})
+</script>
+
+<style scoped>
+.balance-container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  border: solid;
+  margin: 1rem;
+}
+
+.balance-container div {
+  margin: 1rem;
+}
+
+div {
+  margin: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h3 {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/BalanceView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import BalanceItem from './BalanceItem.vue'
+
+const mockData = [
+  { id: '203a871a203a71ads2', date: 'Apr. 31 2024', customerId: '231231', balance: 2434 },
+  {
+    id: '203a871a203a71ads2',
+    date: 'Apr. 31 2024',
+    customerId: '231231',
+    balance: 2434,
+    eventType: 'INCREASED'
+  },
+  {
+    id: '203a871a203a71ads2',
+    date: 'Apr. 31 2024',
+    customerId: '231231',
+    balance: 2434
+  },
+  { id: '203a871a203a71ads2', date: 'Apr. 31 2024', customerId: '231231', balance: 2434 }
+]
+</script>
+
+<template>
+  <div v-for="data in mockData" v-bind:key="data.id">
+    <BalanceItem
+      :eventId="data.id"
+      :date="data.date"
+      :customerId="data.customerId"
+      :balanceValue="data.balance"
+      :eventType="data.eventType"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BalanceView'
+})
+</script>
+
+<style scoped></style>
