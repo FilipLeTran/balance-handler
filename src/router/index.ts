70adf8446fced30480a5d1d2f78f0370e23f6a63
FilipLeTran <filipletran@gmail.com>
2024-04-15 07:58:35 +0200

    fix initial router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import BalanceView from '../views/BalanceView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +7,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: BalanceView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/:retailUnit/:period(\\d{4}-\\d{2})',
+      component: BalanceView,
+      props: true
+    },
+    {
+      path: '/:retailUnit/:period/:businessUnit',
+      component: BalanceView,
+      props: true
     }
   ]
 })
